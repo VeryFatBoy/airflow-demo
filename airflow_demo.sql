f4bf0c569c3c328862a1fe910cd7d0bc76c90e68
VeryFatBoy <VeryFatBoy@users.noreply.github.com>
2024-10-07 03:28:30 +0100

    Create airflow_demo.sql

---
+++ airflow_demo.sql
@@ -0,0 +1,16 @@
+USE airflow_demo;
+
+DROP TABLE IF EXISTS event_stats_staging;
+
+CREATE TABLE event_stats_staging AS
+    SELECT date, user_id, SUM(spend_amt) AS total_spend_amt
+    FROM event
+    WHERE date = '{{ ds }}' AND user_id <> {{ params.test_user_id }}
+    GROUP BY date, user_id;
+
+INSERT INTO event_stats (date, user_id, total_spend_amt)
+    SELECT date, user_id, total_spend_amt
+    FROM event_stats_staging
+    ON DUPLICATE KEY UPDATE total_spend_amt = VALUES(total_spend_amt);
+
+DROP TABLE event_stats_staging;
